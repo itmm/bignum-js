a1ad9eb00f16a2e3d983a484aae6342f0d996f97
Timm Knape <timm.knape@adamhall.com>
2018-01-10 10:02:40 +0100

    better rendering of unit-tests

--- test.js
+++ test.js
@@ -1,25 +1,22 @@
 import * as bn from "./bignum";
 
 let $root;
+let failures = 0;
 
 const test = (source, expected) => {
 	bn.reconfig();
 	const got = eval(source);
-	const $result = document.createElement('DIV');
-	$result.classList.add('test-result');
+	const $result = document.createElement('TR');
 
-	const $source = document.createElement('DIV');
-	$source.classList.add('source');
+	const $source = document.createElement('TD');
 	$source.appendChild(document.createTextNode(source));
 	$result.appendChild($source);
 
-	const $got = document.createElement('DIV');
-	$got.classList.add('got');
+	const $got = document.createElement('TD');
 	$got.appendChild(document.createTextNode(got));
 	$result.appendChild($got);
 
-	const $expected = document.createElement('DIV');
-	$expected.classList.add('expected');
+	const $expected = document.createElement('TD');
 	$expected.appendChild(document.createTextNode(expected));
 	$result.appendChild($expected);
 
@@ -27,6 +24,7 @@ const test = (source, expected) => {
 		$result.classList.add('ok');
 	} else {
 		$result.classList.add('failed');
+		++failures;
 	}
 	$root.appendChild($result);
 }
@@ -38,7 +36,7 @@ window.addEventListener('load', (event) => {
 	test("bn.toString(bn.fromNumber(1234))", "1 234");
 	test("bn.toString(bn.fromString('1234'))", "1 234");
 	test("bn.fromString('1 234')", "4321");
-	test("bn.toString(bn.fromString('1 234'))", "1 234");
+	test("bn.toString(bn.fromString('1 234'))", "1 234x");
 	test("bn.toString(bn.fromString(' abc 1+-*[2]34'))", "1 234");
 	test("bn.toString(bn.add(bn.fromNumber(1234), bn.fromNumber(1234)))", "2 468");
 	test("bn.toString(bn.add(bn.fromNumber(1234), bn.fromNumber(2345)))", "3 579");
@@ -55,4 +53,7 @@ window.addEventListener('load', (event) => {
 	test("bn.reconfig('ax'); bn.toString(bn.pow(bn.fromNumber(2), bn.fromNumber(8)))", "256");
 	test("bn.reconfig('ax'); bn.pow(bn.fromNumber(2), bn.fromNumber(8))", "aaaaaaaax");
 	test("bn.reconfig('0123456789abcdef'); bn.fromNumber(0xabcd)", "dcba");
+	document.getElementsByTagName('body')[0].classList.add(
+		failures > 0 ? 'something-failed' : 'all-passed'
+	);
 });
